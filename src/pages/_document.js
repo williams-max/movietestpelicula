fa9f8447743765b4c8ccccf90bbcd049b7c97212
Israel <almanzaisrael75@gmail.com>
2022-10-31 10:47:12 -0400

    update movie 6

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,7 @@ class MyDocument extends Document {
         return (
             <Html>
 
-                <div id="sfctdn8y59klgk93yg8uqzrj1gk4ph74fjh"></div>
-                <script type="text/javascript" src="https://counter5.optistats.ovh/private/counter.js?c=tdn8y59klgk93yg8uqzrj1gk4ph74fjh&down=async" async>
-                </script><noscript><a href="https://www.contadorvisitasgratis.com" title="contador de visitas">
-                    <img src="https://counter5.optistats.ovh/private/contadorvisitasgratis.php?c=tdn8y59klgk93yg8uqzrj1gk4ph74fjh" border="0" title="contador de visitas" alt="contador de visitas" /></a></noscript>
-
-
+       
                 <Head>
 
                     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
